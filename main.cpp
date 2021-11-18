88d2ebf7357be25d1d0327b1a22bf9c7cf1f5d8a
Davy Nash <Davidnash26@gmail.com>
2021-11-18 22:58:10 +0000

    Building out normal distribution function

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 // Simulating a Normal Distribution and plotting in C++
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 float normal_func (float mu, float sigma)
   {
+    double pi = 3.14;
     float result;
-    result = 2*mu*sigma;
+    result = 1/sigma * 1/sqrt(2*pi) * exp(2*mu);
     return result;
   };
 
@@ -17,15 +19,15 @@ int main ()
 
   float final;
 
-  cout << "Enter a mu: " << endl;
+  cout << "Enter a mu: ";
   cin >> mu;
 
-  cout << "Enter a sigma: " << endl;
+  cout << "\nEnter a sigma: ";
   cin >> sigma;
 
   final = normal_func(mu, sigma);
 
-  cout << "Result is: " << final << endl;
+  cout << "\nResult is: " << final << endl;
 
   return 0;
 
